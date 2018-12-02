6daf3957c89994d37855ed47ab53a48b2cc5454c
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-12-02 14:34:40 +0100

    day 1

---
+++ solutions/day1.py
@@ -0,0 +1,26 @@
+lines = [int(line.rstrip('\n')) for line in open('../dataFiles/day1.txt')]
+
+# first part
+startFrequency = 0
+for x in lines:
+    startFrequency = startFrequency + x
+
+print(startFrequency)
+
+
+# second part
+startFrequency = 0
+frequencies = [int(startFrequency)]
+while True:
+    for x in lines:
+        startFrequency = startFrequency + x
+        if startFrequency in frequencies:
+            break
+        else:
+            frequencies.append(startFrequency)
+
+    else:
+        continue
+    break
+
+print(startFrequency)
