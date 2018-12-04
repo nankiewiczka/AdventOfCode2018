0035a0849bf7ae7deb50b4e6786d0940ca7072c6
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-12-05 00:20:40 +0100

    day 3 second part

--- solutions/day3.py
+++ solutions/day3.py
@@ -17,13 +17,33 @@ def main():
 
     print(count_marked_more_than_once(array))
 
+    # second part
+    for line in lines:
+        read_data = extract_data_from_line(line)
+        x = read_data[0]
+        y = read_data[1]
+        width = read_data[2]
+        height = read_data[3]
+        index = read_data[4]
+        flag = True
+        for i in range(x, x + width):
+            for j in range(y, y + height):
+                if not check_if_claim_points_not_overlap(i, j, array):
+                    flag = False
+                    break
+            if not flag:
+                break
+        if flag:
+            print(index)
+
 
 def extract_data_from_line(line):
+    index = int(line[line.index('#')+1:line.index('@')])
     from_left_edge = int(line[line.index('@')+2: line.index(',')])
     from_top_edge = int(line[line.index(',')+1: line.index(':')])
     width = int(line[line.index(':')+1:line.index('x')])
     height = int(line[line.index('x')+1:])
-    return [from_left_edge, from_top_edge, width, height]
+    return [from_left_edge, from_top_edge, width, height, index]
 
 
 def mark_taken_place(x, y, array):
@@ -38,5 +58,8 @@ def count_marked_more_than_once(array):
                 sum += 1
     return sum
 
+
+def check_if_claim_points_not_overlap(x, y, array):
+    return True if array[x][y] == 1 else False
 
 main()
