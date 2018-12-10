8a94a8a1e8645283f0061d8523a890bface13de5
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-12-11 00:35:19 +0100

    day5 part two, not very efficient

--- solutions/day5.py
+++ solutions/day5.py
@@ -23,4 +23,29 @@ while flag:
 print(len(text))
 
 
+# second part
+min_length = len(read_text)
+for letter in [chr(i) for i in range(ord('a'), ord('z')+1)]:
+    text = read_text.replace(letter, "")
+    text = text.replace(chr(ord(letter) - 32), "")
+    flag = True
+    while flag:
+        index = 0
+        length = len(text)
+        flag = False
+        while index < length - 1:
+            if abs(ord(text[index]) - ord(text[index + 1])) == 32:
+                if index + 2 < len(text):
+                    text = text[:index] + text[index + 2:]
+                    index = index + 2
+                else:
+                    text = text[:index]
+                flag = True
+            else:
+                index = index + 1
+            length = len(text)
 
+    if len(text) < min_length:
+        min_length = len(text)
+
+print(min_length)
