20f7c0fdfc99cd1b4c85a182378c0929feb0425c
nankiewiczka <34343185+nankiewiczka@users.noreply.github.com>
2018-12-11 00:37:00 +0100

    Delete day4.py

--- solutions/day4.py
+++
@@ -1,5 +0,0 @@
-# segregujemy po dacie i potem o godzinie
-# wybieramy pomieðzy zaczął i skończył
-# szukamy wszystkich minut gdy spał i wybieramy z najdłuższym przespanym czasem
-# szukamy minuty najczęściej sie powtarzającej
-# pętla przeskakująca przez godziny  inkrementująca minuty
